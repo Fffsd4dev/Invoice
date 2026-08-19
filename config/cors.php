ddba1fb65bf386c4dfdb2a6195849eba59d464ab
Code-Lucius <nemidav@gmail.com>
2026-08-19 12:51:32 +0100

    Fixed cors

---
+++ config/cors.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    'paths' => [
+        'api/*',
+        'image/*',
+        'sanctum/csrf-cookie',
+    ],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'https://invoices.ffsdgroup.com',
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+
+];
